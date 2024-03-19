a46d60e0b3f9e369b3c367a8d1769fe90e9d780e
Diego Fernandez <diegoafanador10@gmail.com>
2024-03-18 23:07:32 -0400

    [FEAT]added navbar and material configuration

---
+++ src/app/components/navbar-web/navbar-web.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar-web',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar-web.component.html',
+  styleUrls: ['./navbar-web.component.scss']
+})
+export class NavbarWebComponent {
+
+}
