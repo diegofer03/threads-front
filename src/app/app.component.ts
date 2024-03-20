165514e789caf4c6b609c242adb9cb4acd8565a4
Diego Fernandez <diegoafanador10@gmail.com>
2024-03-20 12:06:41 -0400

    [FEAT]added logos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AppServiceService } from './services/app-service.service';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { BurgerMenuComponent } from './components/burger-menu/burger-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, BurgerMenuComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/app/components/burger-menu/burger-menu.component.ts
+++ src/app/components/burger-menu/burger-menu.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { cssMenuRight } from '@ng-icons/css.gg'
+import {OverlayModule} from '@angular/cdk/overlay';
 
 @Component({
-  selector: 'app-burger-menu',
+  selector: 'burger-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [NgIconComponent, CommonModule, OverlayModule],
+  viewProviders: [provideIcons({ cssMenuRight })],
   templateUrl: './burger-menu.component.html',
   styleUrls: ['./burger-menu.component.scss']
 })
 export class BurgerMenuComponent {
 
+  isOpen: boolean = false;
 }
