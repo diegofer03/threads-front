4526190ba2142c371f28ede9538041f614845ec5
Diego Fernandez <diegoafanador10@gmail.com>
2024-03-23 01:40:24 -0400

    [FEAT]added styles dialog create

---
+++ src/app/components/create-thread/dialog/dialog.component.ts
@@ -0,0 +1,28 @@
+import { Component, Inject, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DIALOG_DATA} from '@angular/cdk/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import { AppServiceService } from 'src/app/services/app-service.service';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-dialog',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule, ReactiveFormsModule],
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss']
+})
+export class DialogComponent {
+  private appService = inject(AppServiceService)
+  darkMode = this.appService.darkMode
+  constructor(@Inject(DIALOG_DATA) public data: any) {}
+
+  threadContent = new FormControl('',[
+    Validators.required,
+  ])
+
+  createThread(){
+    if(this.threadContent.valid){
+      console.log(this.threadContent.value)
+    }
+  }
+}
