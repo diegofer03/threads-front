5895ee74d5f01068550ddc8cce61b1e4453cd176
Diego Fernandez <diegoafanador10@gmail.com>
2024-03-22 21:44:02 -0400

    [FEAT]added select theme in burguer menu

--- src/app/services/app-service.service.ts
+++ src/app/services/app-service.service.ts
@@ -20,16 +20,47 @@ export class AppServiceService {
 
   manageDarkMode(): boolean{
     const darkKey = window.localStorage.getItem('darkMode')
-    console.log(darkKey)
     if(!darkKey){
-      console.log('vacio')
-      console.log(darkKey)
       window.localStorage.setItem('darkMode', 'system')
       return window.matchMedia('(prefers-color-scheme: dark)').matches
     }else{
       if(darkKey === 'enable' ) return true
       else if (darkKey === 'disable') return false
       else return window.matchMedia('(prefers-color-scheme: dark)').matches
+    }
+  }
+
+  setTheme(mode: 'light' | 'dark' | 'system'){
+    switch (mode) {
+      case 'light':
+        this.darkMode.set(false)
+        window.localStorage.setItem('darkMode', 'disable')
+        break;
+      case 'dark':
+        this.darkMode.set(true)
+        window.localStorage.setItem('darkMode', 'enable')
+        break;
+      case 'system':
+        this.darkMode.set(window.matchMedia('(prefers-color-scheme: dark)').matches)
+        window.localStorage.setItem('darkMode', 'system')
+        break
+      default:
+        break;
+    }
+  }
+
+  getCurrentTheme() {
+    const theme = window.localStorage.getItem('darkMode')
+    switch (theme) {
+      case 'disable':
+        return 'light'
+      case 'enable':
+
+        return 'dark';
+      case 'system':
+        return 'system'
+      default:
+        return 'system';
     }
   }
 }
