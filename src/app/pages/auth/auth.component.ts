6a1b569814540a2bed6cfd357f983292aefa5a33
Diego Fernandez <diegoafanador10@gmail.com>
2024-03-23 22:57:37 -0400

    [FEAT]added login layout

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppServiceService } from 'src/app/services/app-service.service';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent {
+  appService = inject(AppServiceService)
+  formBuilder = inject(FormBuilder)
+  darkMode = this.appService.darkMode
 
+  loginForm = this.formBuilder.nonNullable.group({
+    user: ['', [ Validators.required]],
+    password: ['', [ Validators.required, Validators.minLength(6)]],
+  });
+
+  login(){
+    const { user, password } = this.loginForm.getRawValue()
+    console.log(user, password)
+  }
 }
