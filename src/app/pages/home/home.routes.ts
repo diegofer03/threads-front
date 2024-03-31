8f7c46c27c7a1b275c3547b033ea2d97cef7221a
Diego Fernandez <diegoafanador10@gmail.com>
2024-03-30 23:19:01 -0400

    [FEAT]added thread view

--- src/app/pages/home/home.routes.ts
+++ src/app/pages/home/home.routes.ts
@@ -15,6 +15,11 @@ export default [
     path: 'search',
     loadComponent: () => import('./search/search.component').then(s => s.SearchComponent),
     title: 'Search • Threads'
+  },
+  {
+    path: 'post',
+    loadComponent: () => import ('./thread/thread.component').then(t => t.ThreadComponent),
+    title: 'post'
   }
   // {path: 'login', pathMatch : 'full', redirectTo: ''},
 ] as Routes ;
---
+++ src/app/pages/home/thread/thread.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-thread',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './thread.component.html',
+  styleUrls: ['./thread.component.scss']
+})
+export class ThreadComponent {
+
+}
