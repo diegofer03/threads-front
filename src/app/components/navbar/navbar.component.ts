8b9938b3eb38663a9c5b3de311016cc604becc62
Diego Fernandez <diegoafanador10@gmail.com>
2024-03-31 22:06:26 -0400

    [FEAT]added navbar return home arrow button

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -4,20 +4,22 @@ import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import {Dialog, DialogModule} from '@angular/cdk/dialog';
 import { heroHomeMini } from '@ng-icons/heroicons/mini';
 import { heroUser, heroMagnifyingGlass, heroHeart, heroPencilSquare } from '@ng-icons/heroicons/outline'
+import {heroArrowLeftSolid} from '@ng-icons/heroicons/solid'
 import { DialogComponent } from '../create-thread/dialog/dialog.component';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { SessionService } from 'src/app/services/session/session.service';
 
 @Component({
   selector: 'navbar',
   standalone: true,
   imports: [NgIconComponent, CommonModule, DialogModule, RouterLink , RouterLinkActive],
-  viewProviders: [provideIcons({ heroHomeMini, heroUser, heroMagnifyingGlass, heroHeart, heroPencilSquare })],
+  viewProviders: [provideIcons({ heroHomeMini, heroUser, heroMagnifyingGlass, heroHeart, heroPencilSquare, heroArrowLeftSolid })],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
   private sessionService = inject(SessionService)
+  private router = inject(Router)
   user = this.sessionService.user
 
   constructor(public dialog: Dialog) {}
@@ -30,6 +32,10 @@ export class NavbarComponent {
         animal: 'panda',
       },
     });
+  }
+
+  return(){
+    this.router.navigate(['/home'])
   }
 
 }
