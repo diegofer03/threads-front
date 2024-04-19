bb7bef2e142c8aa6ff394fcc970f4c0264251c63
Diego Fernandez <diegoafanador10@gmail.com>
2024-04-18 22:14:01 -0400

    [TEST]added test for creat thread dialog

--- src/app/components/create-thread/create-thread.component.spec.ts
+++ src/app/components/create-thread/create-thread.component.spec.ts
@@ -4,27 +4,32 @@ import { CreateThreadComponent } from './create-thread.component';
 import { SessionService } from 'src/app/services/session/session.service';
 import { AppServiceService } from 'src/app/services/app/app-service.service';
 import { By } from '@angular/platform-browser';
+import { Dialog } from '@angular/cdk/dialog';
 
-fdescribe('CreateThreadComponent', () => {
+describe('CreateThreadComponent', () => {
   let component: CreateThreadComponent;
   let fixture: ComponentFixture<CreateThreadComponent>;
   let sessionService: jasmine.SpyObj<SessionService>
   let appService: jasmine.SpyObj<AppServiceService>
+  let dialog: jasmine.SpyObj<Dialog>
 
   beforeEach(() => {
     const spySessionService = jasmine.createSpyObj<SessionService>('SessionService', ['user'])
     const spyAppService = jasmine.createSpyObj<AppServiceService>('AppService', ['darkMode'])
+    const spyDialog = jasmine.createSpyObj('Dialog', ['open'])
 
     TestBed.configureTestingModule({
       imports: [CreateThreadComponent],
       providers: [
         {provide: SessionService, useValue: spySessionService},
-        {provide: AppServiceService, useValue: spyAppService}
+        {provide: AppServiceService, useValue: spyAppService},
+        // {provide: Dialog, useValue: spyDialog}
       ]
     });
     fixture = TestBed.createComponent(CreateThreadComponent);
     sessionService = TestBed.inject(SessionService) as jasmine.SpyObj<SessionService>
     appService = TestBed.inject(AppServiceService) as jasmine.SpyObj<AppServiceService>
+    // dialog = TestBed.inject(Dialog) as jasmine.SpyObj<Dialog>
     component = fixture.componentInstance;
     sessionService.user.and.returnValue({
       _id: '1234',
@@ -49,4 +54,11 @@ fdescribe('CreateThreadComponent', () => {
     const imgElement: HTMLImageElement = fixture.debugElement.query(By.css('#user_img')).nativeElement
     expect(imgElement.src).toContain(component.user()!.name)
   })
+
+  // it('should call to open dialog', () => {
+  //   dialog.open.and.returnValue()
+  //   component.openCreateDialog()
+  //   fixture.detectChanges()
+  //   expect(dialog.open).toHaveBeenCalled()
+  // })
 });
--- src/app/components/create-thread/dialog/dialog.component.spec.ts
+++ src/app/components/create-thread/dialog/dialog.component.spec.ts
@@ -1,17 +1,51 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DialogComponent } from './dialog.component';
+import { SessionService } from 'src/app/services/session/session.service';
+import { AppServiceService } from 'src/app/services/app/app-service.service';
+import { FeedService } from 'src/app/services/feed/feed.service';
+import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
 
-describe('DialogComponent', () => {
+fdescribe('DialogComponent', () => {
   let component: DialogComponent;
   let fixture: ComponentFixture<DialogComponent>;
+  let sessionService: jasmine.SpyObj<SessionService>
+  let appService: jasmine.SpyObj<AppServiceService>
+  let feedService: jasmine.SpyObj<FeedService>
 
   beforeEach(() => {
+    const spySessionService = jasmine.createSpyObj<SessionService>('SessionService', ['user'])
+    const spyAppService = jasmine.createSpyObj<AppServiceService>('AppService', ['darkMode'])
+    const spyFeedService = jasmine.createSpyObj<FeedService>('FeedService', ['createThread'])
+
     TestBed.configureTestingModule({
-      imports: [DialogComponent]
+      imports: [DialogComponent],
+      providers: [
+        {provide: SessionService, useValue: spySessionService},
+        {provide: AppServiceService, usevalue: spyAppService},
+        {provide: FeedService, useValue: spyFeedService},
+        { provide: DialogRef, useValue: {} },
+        { provide: DIALOG_DATA,  useValue: {} }
+      ]
     });
     fixture = TestBed.createComponent(DialogComponent);
     component = fixture.componentInstance;
+    sessionService = TestBed.inject(SessionService) as jasmine.SpyObj<SessionService>
+    appService = TestBed.inject(AppServiceService) as jasmine.SpyObj<AppServiceService>
+    feedService =  TestBed.inject(FeedService) as jasmine.SpyObj<FeedService>
+
+    sessionService.user.and.returnValue({
+      _id: '1234',
+      name: 'alex',
+      email: 'alex@mail.com',
+      userName: 'alex22',
+    })
+    component.user = {
+      _id: '1234',
+      name: 'alex',
+      email: 'alex@mail.com',
+      userName: 'alex22',
+    }
     fixture.detectChanges();
   });
 
